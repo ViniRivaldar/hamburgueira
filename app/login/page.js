0d868f42fdc61622369c38ec9e3c678ba479b19a
Vinicius Rivaldar dos Santos Silva <149613214+ViniRivaldar@users.noreply.github.com>
2024-12-19 00:20:49 +0000

    criando pagina de login

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,29 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import FormLogin from "../pages/FormLogin"
+
 export default function Login(){
-    return <h1>login</h1>
+    return(
+        <main
+            className="w-full flex flex-col justify-center items-center"
+            style={{
+                backgroundImage: "url('/background.png')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                width: '100vw',
+                height: "100vh",
+            }}
+        >
+            <section className="flex rounded-lg bg-[#373737] w-[1078px] h-[664px]">
+                <Image src='/login.png' alt='login' width={539} height={664}/>
+                <div className="flex flex-col items-center w-[539px] h-[664px]">
+                    <Image src='/logo.png' alt='' width={260} height={94} className="mt-14"/>
+                    <h1 className="mt-32 mb-10 text-white text-[24px]">Login</h1>
+                    <FormLogin/>
+                    <p className='mr-[120px]  text-white'>Não possui uma conta? <Link href='/register' className="hover:border-b">Registrar-se</Link></p>
+                </div>
+            </section>
+        </main>
+    )
 }
--- app/pages/FormLogin.js
+++ app/pages/FormLogin.js
@@ -7,17 +7,17 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function FormRegister() {
+export default function FormLogin() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const router = useRouter();
 
     const onSubmit = async (data) => {
         try {
-            const response = await AxiosAuth.post('/register', data);
-            toast.success('Cadastro realizado com sucesso!');
+            const response = await AxiosAuth.post('/login', data);
+            toast.success('Seja bem-vindo ao CodeBurguer!');
             setTimeout(() => {
-                router.push('/login');
-            }, 2000);
+                router.push('/');
+            },2000)
         } catch (e) {
             if (e.response?.data?.errors) {
                 e.response.data.errors.forEach(errorMessage => {
@@ -29,7 +29,7 @@ export default function FormRegister() {
                     }
                 });
             } else {
-                toast.error('Erro ao realizar o cadastro. Tente novamente.');
+                toast.error('Erro ao realizar o login. Tente novamente.');
             }
         }
     }
@@ -44,14 +44,7 @@ export default function FormRegister() {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center gap-4'>
-            <div className='text-white flex flex-col'>
-                <span>Nome:</span>
-                <input
-                    type="text"
-                    {...register('name', { required: 'Nome é obrigatório' })}
-                    className={`w-[391.416px] h-[30px] rounded-[5px] text-black ${errors.name ? 'border-2 border-red-500 focus:border-red-500 focus:ring-red-500' : 'border border-gray-300'}`}
-                />
-            </div>
+            
 
             <div className='text-white flex flex-col'>
                 <span>Username:</span>
@@ -59,21 +52,6 @@ export default function FormRegister() {
                     type="text"
                     {...register('username', { required: 'Username é obrigatório' })}
                     className={`w-[391.416px] h-[30px] rounded-[5px] text-black ${errors.username ? 'border-2 border-red-500' : ''}`}
-                />
-            </div>
-
-            <div className='text-white flex flex-col'>
-                <span>Email:</span>
-                <input
-                    type="email"
-                    {...register('email', {
-                        required: 'Email é obrigatório',
-                        pattern: {
-                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                            message: 'Formato de email inválido',
-                        }
-                    })}
-                    className={`w-[391.416px] h-[30px] rounded-[5px] text-black ${errors.email ? 'border-2 border-red-500' : ''}`}
                 />
             </div>
 
@@ -95,24 +73,8 @@ export default function FormRegister() {
                     className={`w-[391.416px] h-[30px] rounded-[5px] text-black ${errors.password ? 'border-2 border-red-500' : ''}`}
                 />
             </div>
-
-            <div className='text-white flex flex-col'>
-                <span>WhatsApp:</span>
-                <input
-                    type="text"
-                    {...register('phone_number', {
-                        required: 'Telefone é obrigatório',
-                        pattern: {
-                            value: /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/,
-                            message: 'Formato de telefone inválido',
-                        }
-                    })}
-                    className={`w-[391.416px] h-[30px] rounded-[5px] text-black ${errors.phone_number ? 'border-2 border-red-500' : ''}`}
-                />
-            </div>
-
-            <button type="submit" className='w-[182.81px] h-[36.129px] bg-[#9758A6] rounded-full mt-[10] mr-[208.61px] text-white text-[16px] hover:bg-[#844C8C] hover:shadow-lg transition-all duration-300'>
-                Cadastar
+            <button type="submit" className='w-[182.81px] h-[36.129px] bg-[#9758A6] rounded-full mt-[50px] mr-[208.61px] text-white text-[16px] hover:bg-[#844C8C] hover:shadow-lg transition-all duration-300'>
+                Entrar
             </button>
             <ToastContainer />
         </form>
